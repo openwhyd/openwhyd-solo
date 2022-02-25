32f21ab4be56362d5bf10492929aa2aef1635a82
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-02-25 14:23:57 +0100

    first openwhyd approval test: "When posting a track, should respond with the track data"

--- test/approval/approval.tests.js
+++ test/approval/approval.tests.js
@@ -1,7 +1,8 @@
 // Run with: $ npm run test:approval
 
 require('approvals').mocha();
-
+const util = require('util');
+const api = require('../api-client');
 const {
   readMongoDocuments,
   insertTestData,
@@ -14,25 +15,26 @@ const {
   DONT_KILL,
 } = process.env;
 
+const URL_PREFIX = process.env.URL_PREFIX || 'http://localhost:8080';
+
 const MONGODB_URL =
   process.env.MONGODB_URL || 'mongodb://localhost:27117/openwhyd_test';
 
 const context = {};
 
 before(async () => {
-  const testDataCollections = {
+  context.testDataCollections = {
     user: await readMongoDocuments(__dirname + '/../approval.users.json.js'),
     post: await readMongoDocuments(__dirname + '/../approval.posts.json.js'),
   };
-  await insertTestData(MONGODB_URL, testDataCollections);
+  await insertTestData(MONGODB_URL, context.testDataCollections);
 
   context.serverProcess = await startOpenwhydServer({
     startWithEnv: START_WITH_ENV_FILE,
     port: PORT,
   });
-  context.openwhyd = require('../api-client');
   context.getUser = (id) =>
-    testDataCollections.user.find(({ _id }) => id === _id.toString());
+    context.testDataCollections.user.find(({ _id }) => id === _id.toString());
 });
 
 after(() => {
@@ -41,9 +43,31 @@ after(() => {
   }
 });
 
-describe('When running some tests', function () {
-  it('should be able to use Approvals', function () {
-    var data = 'Hello World!';
-    this.verify(data); // or this.verifyAsJSON(data)
+describe('When posting a track', function () {
+  let postedTrack;
+
+  before(async () => {
+    const user = context.testDataCollections.user[0];
+    const post = {
+      uId: user._id,
+      uNm: user.name,
+      text: '',
+      name: 'BOYLE - Roppongi Hills (Music Video)',
+      eId: '/yt/jI3YrVfOksE',
+      ctx: 'bk',
+      img: 'https://i.ytimg.com/vi/jI3YrVfOksE/default.jpg',
+      src: {
+        id: 'https://www.youtube.com/watch?v=jI3YrVfOksE',
+        name: 'BOYLE - Roppongi Hills (Music Video) - YouTube',
+      },
+    };
+    const { jar } = await util.promisify(api.loginAs)(user);
+    postedTrack = (await util.promisify(api.addPost)(jar, post)).body;
   });
+
+  it('should respond with the track data', function () {
+    this.verifyAsJSON(postedTrack); // or this.verify(data)
+  });
+
+  // TODO: it('should be listed in the "post" db collection', function () {
 });

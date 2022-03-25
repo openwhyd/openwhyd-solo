7f8cd91958bb05fcf8b94d567f57cff7408129cf
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-03-25 10:23:38 +0100

    goal: Simplify userCollection.insertPlaylist() implementation

--- app/infrastructure/mongodb/UserCollection.js
+++ app/infrastructure/mongodb/UserCollection.js
@@ -6,8 +6,9 @@
  * @typedef {import('../../../app/domain/user/types').Playlist} Playlist
  */
 
-const { fetchByUid, save } = require('../../models/user');
+const { fetchByUid } = require('../../models/user');
 const User = require('../../domain/user/User');
+const mongodb = require('../../models/mongodb');
 
 /**
  * @type {UserRepository}
@@ -17,13 +18,10 @@ exports.userCollection = {
     new Promise((resolve) => fetchByUid(userId, mapToDomainUser(resolve))),
 
   insertPlaylist: (userId, playlist) =>
-    new Promise((resolve) =>
-      /*
-       We are forced to use users.fetchByUid() then users.save() otherwise the database model migration won't be performed.
-       But it can be replaced by the following code, if we implement a database migration that adds (id, mid, n, prefs) to all users in the database:
-
-       mongodb.collections['user'].updateOne(
-        { _id: ObjectId(userId) },
+    // TODO: return updateOne() function's promise directly, if possible, instead of recreating our own
+    new Promise((resolve, reject) =>
+      mongodb.collections['user'].updateOne(
+        { _id: mongodb.ObjectId(userId) },
         { $push: { pl: playlist } },
         (err, result) => {
           if (err) {
@@ -31,16 +29,8 @@ exports.userCollection = {
           } else {
             resolve(result);
           }
-        })
-
-       */
-      fetchByUid(userId, (user) => {
-        user.pl = user.pl || [];
-        //@ts-ignore
-        user.pl.push(playlist);
-        //@ts-ignore
-        save(user, resolve);
-      })
+        }
+      )
     ),
 };
 
